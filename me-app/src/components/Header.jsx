7f25022eb0cc5c3f3bb3fa1a03c4f23bb868e8c3
mal4043 <mal6191988@gmail.com>
2023-05-18 12:52:35 +0300

    updated Header.jsx resized links

--- me-app/src/components/Header.jsx
+++ me-app/src/components/Header.jsx
@@ -6,6 +6,11 @@ function Header() {
     return (
         <div className="header">
             <h1>Header!!!</h1>
+            <ul>
+                <a href="#"><li>red</li> </a>
+                <a href="#"><li>yellow</li> </a>
+                <a href="#"><li>green</li> </a>
+            </ul>
             <p>to check</p>
         </div>
     )
